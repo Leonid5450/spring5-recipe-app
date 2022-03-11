6eced9a31d92ea48ed6eae31dc056d647a2a8051
ziskle20 <ziskle20@SHS5099433C.cable.rcn.com>
2022-03-10 20:58:42 -0600

    add-index-page

--- src/main/java/guru/springframework/controllers/IndexController.java
+++ src/main/java/guru/springframework/controllers/IndexController.java
@@ -1,7 +1,14 @@
 package guru.springframework.controllers;
 
+import java.util.Optional;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import guru.springframework.domain.Category;
+import guru.springframework.domain.UnitOfMeasure;
+import guru.springframework.repositories.CategoryRepository;
+import guru.springframework.repositories.UnitOfMeasreRepository;
 
 /**
  * Created by jt on 6/1/17.
@@ -9,8 +16,29 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class IndexController {
 
-    @RequestMapping({"", "/", "/index"})
+	private CategoryRepository categoryRepository;
+	private UnitOfMeasreRepository unitOfMeasreRepository;
+	
+	
+	
+    public IndexController(CategoryRepository categoryRepository, UnitOfMeasreRepository unitOfMeasreRepository) {
+		super();
+		this.categoryRepository = categoryRepository;
+		this.unitOfMeasreRepository = unitOfMeasreRepository;
+	}
+
+   
+
+	@RequestMapping({"", "/", "/index"})
     public String getIndexPage(){
+		
+		System.out.println("Category id = ");
+		
+	Optional<Category> categoryOptional = categoryRepository.findByDescription("American");
+	Optional<UnitOfMeasure> unitOfMeasureOptional = unitOfMeasreRepository.findByDescription("Teaspoon");
+	
+	System.out.println("Category id = " + categoryOptional.get().getId());
+	System.out.println("Unit of measure id =" + unitOfMeasureOptional.get().getId());
         return "index";
     }
 }
---
+++ src/main/java/guru/springframework/repositories/UnitOfMeasreRepository.java
@@ -0,0 +1,13 @@
+package guru.springframework.repositories;
+
+import java.util.Optional;
+
+import org.springframework.data.repository.CrudRepository;
+
+import guru.springframework.domain.UnitOfMeasure;
+
+public interface UnitOfMeasreRepository extends CrudRepository<UnitOfMeasure, Long> {
+	
+	Optional<UnitOfMeasure> findByDescription (String description);
+
+}
